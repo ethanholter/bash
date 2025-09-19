317cb01a455438a63f9aa4bdde9e17e1c40b20b1
Ethan Holter <holterethan@gmail.com>
2025-09-19 00:03:45 -0500

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,26 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-if [ -f "${HOME}/.bashrc" ]; then
-    mv "${HOME}/.bashrc" "${HOME}/.bashrc.old" 
-fi
-
+# clone/pull config from github
 if [ ! -d "${HOME}/.config/bash" ]; then
     git clone https://github.com/ethanholter/bash "${HOME}/.config/bash"
 else
     (cd ${HOME}/.config/bash && git pull)
 fi
 
-if [ ! -f "${HOME}/.bashrc" ]; then
-    ln -s "${HOME}/.config/bash/.bashrc" "${HOME}/.bashrc"
+# symlink already exists
+if [ -L "${HOME}/.bashrc" ]; then 
+    echo "Config already in use. Updated from remote"
+    exit 0
 fi
+
+# regular config already exists. Make a backup
+if [ -f "${HOME}/.bashrc" ]; then
+    mv "${HOME}/.bashrc" "${HOME}/.bashrc.old" 
+fi
+
+# symlink config
+ln -s "${HOME}/.config/bash/.bashrc" "${HOME}/.bashrc"
 
 echo "success"
 
